406ae1df546447de397db48f6f3d994f8a51a360
Zuhaq95 <77640961+Zuhaq95@users.noreply.github.com>
2026-08-01 18:23:54 +0500

    Phase 1 completed where we are collecting the creators and dumping in csv

--- config.py
+++ config.py
@@ -5,3 +5,15 @@ DEBUG = True
 SHOP_REGION = "GB"
 
 HEADLESS = False
+
+# ------------------------------------
+# Creator Collection
+# ------------------------------------
+
+MAX_CREATORS = 30
+
+SCROLL_DISTANCE = 900
+
+SCROLL_WAIT_MS = 1500
+
+MAX_EMPTY_SCROLLS = 3
